5693a676c40546123932034d5134146ad00f9d9e
Mikhail Makarov <makarovmma@gmail.com>
2021-11-14 14:14:20 +0300

    Implement process sequence

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,88 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
-import Api from '../tools/api';
+import Api from "../tools/api";
+import {compose, tap, andThen} from "ramda";
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+const processSequence = async ({
+  value,
+  writeLog,
+  handleSuccess,
+  handleError
+}) => {
+  const validate = (s) => {
+    return (
+      s.length < 10 &&
+      s.length > 2 &&
+      /^[0-9]*\.?[0-9]*$/.test(s) &&
+      Number(value) > 0
+    );
+  };
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+  const validateValue = (value) => {
+    if (!validate(value)) return handleError("ValidationError");
+  };
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
+  const roundNumber = (value) => {
+    const floorDif = Number(value) - Math.floor(Number(value));
+    const ceilDif = Math.ceil(Number(value)) - Number(value);
+    return floorDif < ceilDif
+      ? Math.floor(Number(value))
+      : Math.ceil(Number(value));
+  };
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+  const transformNumber = (number) =>
+    new Promise((res, rej) =>
+      api
+        .get("https://api.tech/numbers/base", {from: 10, to: 2, number})
+        .then(({result}) => {
+          res(result);
+        })
+    );
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+  const getLength = (s) => s.toString().length;
+  const square = (a) => a * a;
+  const modByThree = (a) => a % 3;
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+  const getAnimal = (id) =>
+    new Promise((res, rej) =>
+      api.get(`https://animals.tech/${id}`, {}).then(({result}) => {
+        res(result);
+      })
+    );
+
+  compose(
+    andThen(handleSuccess),
+    andThen(tap(writeLog)),
+    andThen(getAnimal),
+    andThen(tap(writeLog)),
+    andThen(modByThree),
+    andThen(tap(writeLog)),
+    andThen(square),
+    andThen(tap(writeLog)),
+    andThen(getLength),
+    andThen(tap(writeLog)),
+    transformNumber,
+    tap(writeLog),
+    roundNumber,
+    tap(validateValue),
+    tap(writeLog)
+  )(value);
+};
 
 export default processSequence;
